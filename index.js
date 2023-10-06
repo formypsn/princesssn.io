f37479b16285e21413b8418d4bc2a8529acec90c
formypsn <146628327+formypsn@users.noreply.github.com>
2023-10-06 22:23:54 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var password = "psngp"
+
+function passcheck() {
+
+	if(document.getElementById('pass1').value !=password) {
+
+		alert('Wrong pass');
+
+		return false;
+	}
+
+	if(document.getElementById('pass1').value == password) {
+
+		
+	}
+}
